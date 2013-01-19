c7bc1f80d8727d5cde47b74c1a1e2a0ff8581504
Robert Li <li.robertj+git@gmail.com>
2013-01-19 00:24:11 -0500

    Updated database schema again

--- SplitIt/src/com/fivepoundshakes/splitit/MainActivity.java
+++ SplitIt/src/com/fivepoundshakes/splitit/MainActivity.java
@@ -1,11 +1,14 @@
 package com.fivepoundshakes.splitit;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 import com.stackmob.android.sdk.common.StackMobAndroid;
+import com.stackmob.sdk.callback.StackMobModelCallback;
+import com.stackmob.sdk.exception.StackMobException;
 
 public class MainActivity extends Activity {
 
@@ -13,7 +16,66 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StackMobAndroid.init(getApplicationContext(), 0, "ce86bf3d-f248-4c91-ac03-10e4244faa4c");
+        StackMobAndroid.init(getApplicationContext(), 0,
+                "ce86bf3d-f248-4c91-ac03-10e4244faa4c");
+        
+        // Testing User
+        User u = new User("robert", "Robert", "Li", "robertli");
+        u.save(new StackMobModelCallback() {
+            @Override
+            public void failure(StackMobException arg0) {
+                System.out.println("Failure: " + arg0.getMessage());
+            }
+            
+            @Override
+            public void success() {
+                System.out.println("Success!");
+            }
+        });
+        
+        // Make more!
+        User u1 = new User("jean", "Jean", "Kim", "jeankim");
+        u1.save(new StackMobModelCallback() {
+            @Override
+            public void failure(StackMobException arg0) {
+                System.out.println("Failure: " + arg0.getMessage());
+            }
+            
+            @Override
+            public void success() {
+                System.out.println("Success!");
+            }
+        });
+        
+        User u2 = new User("aimee", "Aimee", "Kim", "aimeekim");
+        u2.save(new StackMobModelCallback() {
+            @Override
+            public void failure(StackMobException arg0) {
+                System.out.println("Failure: " + arg0.getMessage());
+            }
+            
+            @Override
+            public void success() {
+                System.out.println("Success!");
+            }
+        });
+        
+        // Test Expense
+        ArrayList<User> parties = new ArrayList<User>();
+        parties.add(u1);
+        parties.add(u2);
+        Expense e = new Expense(u, parties, "McDonald's", 50, "50 McChickens!");
+        e.save(new StackMobModelCallback() {
+            @Override
+            public void failure(StackMobException arg0) {
+                System.out.println("Failure: " + arg0.getMessage());
+            }
+            
+            @Override
+            public void success() {
+                System.out.println("Success!");
+            }
+        });
     }
 
     @Override
@@ -22,5 +84,5 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+
 }
