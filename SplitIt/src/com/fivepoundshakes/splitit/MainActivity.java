2454329403f648fba50fd1daf7d548346dee20c3
secretasianman <nobatteriesincluded@gmail.com>
2013-01-19 04:17:53 -0500

    Basic Venmo functionality.

--- SplitIt/src/com/fivepoundshakes/splitit/MainActivity.java
+++ SplitIt/src/com/fivepoundshakes/splitit/MainActivity.java
@@ -7,17 +7,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.fivepoundshakes.splitit.VenmoLibrary.VenmoResponse;
 import com.stackmob.android.sdk.common.StackMobAndroid;
 import com.stackmob.sdk.api.StackMobQuery;
 import com.stackmob.sdk.callback.StackMobQueryCallback;
 import com.stackmob.sdk.exception.StackMobException;
 
 public class MainActivity extends Activity {
+	final String VENMO_APP_ID = "1219";
+	final String VENMO_APP_SECRET = "Xzygvtey8MhKw9Fgf3PdNHMkxLZfU9pw";
+	final String VENMO_APP_NAME = "SplitIt";
+	final String VENMO_TEST_TXN = "pay";
+	
+	//Test vars
+	String VENMO_TEST_RECIPIENT = "robertli";
+	String VENMO_TEST_AMT = "1";
+	String VENMO_TEST_NOTE = "Testing this shit, foo!";
+	
 
     User self;
     String serial;
@@ -99,6 +111,53 @@ public class MainActivity extends Activity {
                 startActivity(i);
             }
         });
+    }
+    
+    /**
+     * Venmo payment flow.
+     */
+    private void venmoInit(String recipient, String amt, String note){
+    	
+        try {
+        	//Needs input from split input page
+            Intent venmoIntent = VenmoLibrary.openVenmoPayment(VENMO_APP_ID, VENMO_APP_NAME, recipient, amt, note, VENMO_TEST_TXN);
+            startActivityForResult(venmoIntent, 1); //1 is the requestCode we are using for Venmo. Feel free to change this to another number. 
+        }
+        catch (android.content.ActivityNotFoundException er) //Venmo native app not install on device, so let's instead open a mobile web version of Venmo in a WebView
+        {
+            Intent venmoIntent = new Intent(MainActivity.this, VenmoWebViewActivity.class);
+            String venmo_uri = VenmoLibrary.openVenmoPaymentInWebView(VENMO_APP_ID, VENMO_APP_NAME, recipient, amt, note, VENMO_TEST_TXN);
+            venmoIntent.putExtra("url", venmo_uri);
+            startActivityForResult(venmoIntent, 1);
+        }
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        switch(requestCode) {
+            case 1: { //1 is the requestCode we picked for Venmo earlier when we called startActivityForResult
+                if(resultCode == RESULT_OK) {
+                    String signedrequest = data.getStringExtra("signedrequest");
+                    if(signedrequest != null) {
+                        VenmoResponse response = (new VenmoLibrary()).validateVenmoPaymentResponse(signedrequest, VENMO_APP_SECRET);
+                        if(response.getSuccess().equals("1")) {
+                            //Payment successful.  Use data from response object to display a success message
+                            String note = response.getNote();
+                            String amount = response.getAmount();
+                        }
+                    }
+                    else {
+                        String error_message = data.getStringExtra("error_message");
+                        //An error occurred.  Make sure to display the error_message to the user
+                    }                               
+                }
+                else if(resultCode == RESULT_CANCELED) {
+                    //The user cancelled the payment
+                }
+            break;
+            }           
+        }
     }
 
 }
