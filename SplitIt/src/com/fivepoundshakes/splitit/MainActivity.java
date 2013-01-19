22852110c86df6208f3e6b6dad0b8028af1f4c7c
Robert Li <li.robertj+git@gmail.com>
2013-01-18 22:25:37 -0500

    Linked in StackMob

--- SplitIt/src/com/fivepoundshakes/splitit/MainActivity.java
+++ SplitIt/src/com/fivepoundshakes/splitit/MainActivity.java
@@ -1,8 +1,11 @@
-package com.example.splitit;
+package com.fivepoundshakes.splitit;
 
 import android.os.Bundle;
+
 import android.app.Activity;
 import android.view.Menu;
+
+import com.stackmob.android.sdk.common.StackMobAndroid;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StackMobAndroid.init(getApplicationContext(), 0, "ce86bf3d-f248-4c91-ac03-10e4244faa4c");
     }
 
     @Override
